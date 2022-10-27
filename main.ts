4d56721d3126303578ee1a9eacea47ebd2777233
wpakching <115679766+wpakching@users.noreply.github.com>
2022-10-27 10:48:29 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+let x = 0
+let y = 0
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        basic.clearScreen()
+        led.plot(x, y)
+        basic.pause(300)
+        x += 1
+    }
+    if (x > 4) {
+        x = 0
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        basic.clearScreen()
+        led.plot(x, y)
+        basic.pause(300)
+        y += 1
+    }
 })
